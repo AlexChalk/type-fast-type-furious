63939b60b435543ecaa0efa58b6472e6c28b735a
Alex Chalk <alexchalk17@gmail.com>
2017-06-06 07:59:32 -0400

    Change the message

--- test/timer.test.js
+++ test/timer.test.js
@@ -5,6 +5,13 @@ describe('Timer', function() {
   //   var fixture = '<div id="fixture"><input id="x" type="text">'
   // });
 
+  beforeEach(function() {
+    document.body.insertAdjacentHTML('afterbegin', '<button id="start">Start</button>')
+  });
+  afterEach(function() {
+    document.body.removeChild(document.getElementById('start'));
+  });
+
   it('should have a start button', function() {
     document.getElementById('start').click();
     expect(window.timer.startTime).toEqual(Date.getTime());

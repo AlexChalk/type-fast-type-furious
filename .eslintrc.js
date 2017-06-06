13af5fb341a54e4303f2b21f5ea1ded7e2569c28
Alex Chalk <alexchalk17@gmail.com>
2017-06-06 06:59:05 -0400

    Configure eslint; initiate timer test file

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  "plugins": ["jasmine"],
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+    "jasmine" :true
+  },
+  "extends": "eslint:recommended",
+  "parserOptions": {
+    "sourceType": "module"
+  },
+  "rules": {
+    "indent": [
+      "error",
+      2
+    ],
+    "linebreak-style": [
+      "error",
+      "unix"
+    ],
+    "quotes": [
+      "error",
+      "single"
+    ],
+    "semi": [
+      "error",
+      "always"
+    ]
+  }
+};

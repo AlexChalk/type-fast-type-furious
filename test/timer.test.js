518713897c685d8ba52eb88d789ca0d1de65dfb0
milesillsley <milesillsley@hotmail.co.uk>
2017-06-06 17:06:02 +0100

    test failing, tried timekeeper

--- test/timer.test.js
+++ test/timer.test.js
@@ -1,7 +1,14 @@
+var tk = require('timekeeper');
+var time = new Date(0);
+var time2 = new Date(1050);
+
 describe('Timer', function() {
 
   beforeEach(function() {
-    document.body.insertAdjacentHTML('afterbegin', '<button id="start">Start</button><button id="stop">Stop</button>');
+    document.body.insertAdjacentHTML('afterbegin',
+      '<button id="start">Start</button>' +
+      '<button id="stop">Stop</button>' +
+      'Total: <span id="total"></span>');
     startListeners();
   });
   afterEach(function() {
@@ -18,5 +25,14 @@ describe('Timer', function() {
     spyOn(Date, 'now').and.returnValue('swish');
     document.getElementById('stop').click();
     expect(timer.stopTime).toEqual('swish');
+  });
+
+  it('displays total time when stopped', function() {
+    tk.freeze(time);
+    document.getElementById('start').click();
+    tk.travel(time2);
+    document.getElementById('stop').click();
+    tk.reset();
+    expect(document.getElementById('total').innerHTML).toEqual(1000);
   });
 });

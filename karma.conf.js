09eb536c5ffdf888071840c0a9e2275d42436f25
david gray <davidseangray@gmail.com>
2017-06-06 16:40:24 +0100

    work check return true if correct input if used!

--- karma.conf.js
+++ karma.conf.js
@@ -24,6 +24,10 @@ module.exports = function(config) {
     exclude: [
     ],
 
+    
+
+
+
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor

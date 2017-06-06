178e349f25c6e8e18804ec920b5eddb6b29a0221
milesillsley <milesillsley@hotmail.co.uk>
2017-06-06 12:42:02 +0100

    failing test for timer start time

--- test/timer.test.js
+++ test/timer.test.js
@@ -1,7 +1,12 @@
 describe('Timer', function() {
 
   // inject the HTML fixture for the tests
-  beforeEach(function() {
-    var fixture = '<div id="fixture"><input id="x" type="text">'
+  // beforeEach(function() {
+  //   var fixture = '<div id="fixture"><input id="x" type="text">'
+  // });
+
+  it('should have a start button', function() {
+    document.getElementById('start').click();
+    expect(window.timer.startTime).toEqual(Date.getTime());
   });
 });

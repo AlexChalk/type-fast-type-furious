579e2cf471c501edf0fc892c2c6321ac83a2de76
david gray <davidseangray@gmail.com>
2017-06-06 13:09:41 +0100

    form displays to the page to take user input

--- test/wordMatcher.test.js
+++ test/wordMatcher.test.js
@@ -1,7 +1,9 @@
-describe('Paragraph', function(){
+describe('wordMatcher', function(){
 
   beforeEach(function(){
-    var fixture = "<div id='fixture'><p id='paragraph'>Hello world</p></div>";
+    var fixture = "<div id='fixture'><p id='paragraph'>Hello world</p>" +
+    '<form><input id="userInput" type="text" name="userInput" placeholder="Start Typing..."></form>' +
+    "</div>";
 
     document.body.insertAdjacentHTML(
       'afterbegin',
@@ -12,9 +14,12 @@ describe('Paragraph', function(){
     document.body.removeChild(document.getElementById('fixture'));
   });
 
-  it('should display the paragraph', function(){
+  it('should display the text', function(){
     expect(document.getElementById('paragraph').innerHTML).toBe('Hello world');
   });
 
-});
+  it('should display a form to take text input', function(){
+    expect(document.getElementById('userInput').innerHTML).toBe('');
+  })
 
+});

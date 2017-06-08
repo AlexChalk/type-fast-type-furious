450f8227193ebfd2e4430a36e42c7c6d8bffb257
milesillsley <milesillsley@hotmail.co.uk>
2017-06-08 11:21:43 +0100

    Ron Swanson api added

--- interactivity.js
+++ interactivity.js
@@ -1,9 +1,19 @@
 $(document).ready(function(){
   'use strict';
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://ron-swanson-quotes.herokuapp.com/v2/quotes', false);
+  xhr.send();
+  var quote = xhr.response.slice(2,-2);
+  //var quote = fetch('http://ron-swanson-quotes.herokuapp.com/v2/quotes');
+  //quote.then(function(data) {
+  //  console.log(data);
+    //console.log(Body.text());
+  //});
+  var wordsToType = quote.split(' ');
 
   this.RandomWordGenerator = new RandomWordGenerator();
 
-  var wordsToType = this.RandomWordGenerator.randomise().split(' ');
+  //var wordsToType = this.RandomWordGenerator.randomise().split(' ');
   wordsToType.forEach(function(word) {
     $('#paragraph').append('<span ' + 'id="' + word + '">' + word + ' </span>');
   });

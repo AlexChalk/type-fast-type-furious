ccf8abeea36853d453c9f7567c0420b9aba85a3b
milesillsley <milesillsley@hotmail.co.uk>
2017-06-09 10:08:11 +0100

    refactoring

--- test/interactivity.test.js
+++ test/interactivity.test.js
@@ -14,30 +14,34 @@ describe('Interactive behaviour', function() {
     expect(formatXHRQuoteToArray(response)).toEqual(['test', 'string']);
   });
 
-  it('creates html span tags with ids for each word', function() {
-    var words = ['test','words'];
-    displayWords(words);
-    expect(document.getElementById('paragraph').innerHTML).toEqual('<span id="0">test </span><span id="1">words </span>');
+  describe('', function() {
+  var words;
+  var completedWords;
+    beforeEach(function() {
+      words = ['test','words'];
+      completedWords = [];
+      displayWords(words);
+    });
+    afterEach(function() {
+      completedWords = [];
+    });
+    it('creates html span tags with ids for each word', function() {
+      expect(document.getElementById('paragraph').innerHTML).toEqual('<span id="0">test </span><span id="1">words </span>');
+    });
+
+    it('markWordAsTyped changes the colour of words', function() {
+      markWordAsTyped(words, 0, completedWords);
+      expect(document.getElementById('0').style.color).toEqual('green');
+    });
+
+    it('markWordAsTyped adds word to completedWords array', function() {
+      markWordAsTyped(words, 0, completedWords);
+      expect(completedWords).toContain('test');
+    });
+    it('assessWords checks two words match', function() {
+      expect(assessWord('test','test')).toBe(true);
+      expect(assessWord('test','stetst')).toBe(false);
+    });
   });
 
-  it('markWordAsTyped changes the colour of words', function() {
-    var words = ['test','words'];
-    displayWords(words);
-    markWordAsTyped(words, 0);
-    expect(document.getElementById('0').style.color).toEqual('green');
-  });
-
-  it('markWordAsTyped adds word to completedWords array', function() {
-    var words = ['test','words'];
-    displayWords(words);
-    markWordAsTyped(words, 0);
-    expect(completedWords).toContain('test');
-  });
-  it('assessWords iterates over typedWords array', function() {
-    var words = ['test','words'];
-    displayWords(words);
-    assessWords(words);
-    expect(completedWords).toContain('test');
-    expect(completedWords).toContain('words');
-  });
 });

9fd32475886824d1e91830cfe5d6a75acb3b1754
milesillsley <milesillsley@hotmail.co.uk>
2017-06-09 15:08:59 +0100

    All tests passing, curley quote bug fixed, words turn red when they are typed wrong

--- test/interactivity.test.js
+++ test/interactivity.test.js
@@ -14,7 +14,12 @@ describe('Interactive behaviour', function() {
     expect(formatXHRQuoteToArray(response)).toEqual(['test', 'string']);
   });
 
-  describe('', function() {
+  it('curlyQuoteRemover replaces curley quotes with straight quotes in a string', function() {
+    expect('‘’“”‘’“”'.removeCurlyQuotes()).toEqual('\'\'""\'\'""');
+    expect('I like saying ‘No,’ it lowers their enthusiasm.'.removeCurlyQuotes()).toEqual("I like saying 'No,' it lowers their enthusiasm.");
+  });
+
+  describe('type assessment', function() {
   var words;
   var completedWords;
     beforeEach(function() {
@@ -29,28 +34,38 @@ describe('Interactive behaviour', function() {
       expect(document.getElementById('paragraph').innerHTML).toEqual('<span id="0">test </span><span id="1">words </span>');
     });
 
-    it('markWordAsTyped changes the colour of words', function() {
-      markWordAsTyped(words, 0, completedWords);
+    it('markWordAsTyped changes the colour of words to green', function() {
+      markWordAsTyped(words, completedWords);
       expect(document.getElementById('0').style.color).toEqual('green');
     });
 
+    it('markWordAsIncorrect changes the colour of words to red', function() {
+      markWordAsIncorrect(words, completedWords);
+      expect(document.getElementById('0').style.color).toEqual('red');
+    });
+
     it('markWordAsTyped adds word to completedWords array', function() {
-      markWordAsTyped(words, 0, completedWords);
+      markWordAsTyped(words, completedWords);
       expect(completedWords).toContain('test');
     });
+
     it('assessWords checks two words match', function() {
       expect(assessWord('test','test')).toBe(true);
       expect(assessWord('test','stetst')).toBe(false);
     });
-    it('assessWord is called with correct words if keypress is space', function() {
-      assessWord = jasmine.createSpy('assessWord');
-//      spyOn(window, 'assessWord');
+
+    it('input is cleared if keypress is space and word matches', function() {
       var userInput = document.getElementById('userInput');
       userInput.value = 'test';
       evaluateKeypress(32, words, completedWords, userInput);
-      expect(assessWord).toHaveBeenCalledWith('test', 'test');
+      expect(userInput.value).toEqual('');
     });
 
+    it('input is not cleared if keypress is space and word does not match', function() {
+      var userInput = document.getElementById('userInput');
+      userInput.value = 'wronglytypedtest';
+      evaluateKeypress(32, words, completedWords, userInput);
+      expect(userInput.value).toEqual('wronglytypedtest');
+    });
   });
-
 });
